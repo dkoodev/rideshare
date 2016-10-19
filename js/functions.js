d1f4d11edc09de62dc08ba861854cc68c41de7f1
Daniel Koo <dkoo.dev@gmail.com>
2016-10-19 15:12:36 -0700

    Start form validation
    Created files for form validation

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,7 @@
-function showSubmitButton{
-	
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
 }
